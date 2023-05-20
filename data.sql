c2caca1166a4bf3edc1361abcf5bcba4d7ccbd24
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-05-20 16:12:58 +0530

    multiple table and query added

--- data.sql
+++ data.sql
@@ -26,3 +26,37 @@ VALUES ('Blossom', DATE '1998-10-13', 3, true, 17);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Ditto', DATE '2022-05-14', 4, true, 22);
+
+INSERT INTO owners (full_name, age) VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+-- Insert data into the species table
+INSERT INTO species (name) VALUES
+  ('Pokemon'),
+  ('Digimon');
+
+-- Modify inserted animals to include the species_id value
+UPDATE animals
+SET species_id = CASE
+  WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+  ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+  END;
+
+-- Modify inserted animals to include owner information (owner_id)
+UPDATE animals AS a
+SET owner_id = o.id
+FROM owners AS o
+WHERE a.owner_id IS NULL
+  AND (
+    (a.name = 'Agumon' AND o.full_name = 'Sam Smith') OR
+    (a.name IN ('Devimon', 'Plantmon') AND o.full_name = 'Bob') OR
+    (a.name IN ('Charmander', 'Squirtle', 'Blossom') AND o.full_name = 'Melody Pond') OR
+    (a.name IN ('Gabumon', 'Pikachu') AND o.full_name = 'Jennifer Orwell') OR    
+    (a.name IN ('Angemon', 'Boarmon') AND o.full_name = 'Dean Winchester')
+  );
+  
--- schema.sql
+++ schema.sql
@@ -11,3 +11,21 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(100);
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255),
+  age INTEGER
+);
+
+-- Create the species table
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+-- Modify the table i.e., animals
+ALTER TABLE animals
+  DROP COLUMN species,
+  ADD COLUMN species_id INTEGER REFERENCES species(id),
+  ADD COLUMN owner_id INTEGER REFERENCES owners(id);
